0d6fe074837c9542733abc4d5cadec6cd31e143e
G'lek Tarssza <glek@glektarssza.com>
2023-12-19 13:15:31 -0700

    Added main class.

---
+++ src/main/java/com/glektarssza/creategogglescurio/CreateGogglesCurio.java
@@ -0,0 +1,28 @@
+package com.glektarssza.creategogglescurio;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * The root mod class.
+ */
+@Mod(CreateGogglesCurio.MOD_ID)
+public class CreateGogglesCurio {
+    /**
+     * The ID of the mod.
+     */
+    public static final String MOD_ID = "creategogglescurio";
+
+    /**
+     * The logger to use for the mod.
+     */
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    /**
+     * Create a new instance.
+     */
+    public CreateGogglesCurio() {
+        // TODO
+    }
+}
